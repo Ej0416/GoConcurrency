a02791fb74798f621bb929e0e166148e74e10025
Ej <elnesjani@gmail.com>
2025-10-13 23:19:40 +0800

    lesson 3

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"time"
 )
 
 // func sendEmail(message string) {
@@ -21,35 +20,61 @@ import (
 // 	fmt.Println("========================")
 // }
 
-type email struct {
-	body string
-	date time.Time
-}
+// ------------------------------------ lesson 2
 
-func checkEmailAge(emails [3]email) [3]bool {
-	isOldChan := make(chan bool)
+// type email struct {
+// 	body string
+// 	date time.Time
+// }
 
-	go sendIsOld(isOldChan, emails)
+// func checkEmailAge(emails [3]email) [3]bool {
+// 	isOldChan := make(chan bool)
 
-	isOld := [3]bool{}
-	isOld[0] = <-isOldChan
-	isOld[1] = <-isOldChan
-	isOld[2] = <-isOldChan
-	return isOld
+// 	go sendIsOld(isOldChan, emails)
+
+// 	isOld := [3]bool{}
+// 	isOld[0] = <-isOldChan
+// 	isOld[1] = <-isOldChan
+// 	isOld[2] = <-isOldChan
+// 	return isOld
+// }
+
+// // don't touch below this line
+
+// func sendIsOld(isOldChan chan<- bool, emails [3]email) {
+// 	for _, e := range emails {
+// 		if e.date.Before(time.Date(2020, 0, 0, 0, 0, 0, 0, time.UTC)) {
+// 			isOldChan <- true
+// 			continue
+// 		}
+// 		isOldChan <- false
+// 	}
+// }
+
+func waitForDBs(numDBs int, dbChan chan struct{}) {
+	for range numDBs{
+		<-dbChan
+	}
 }
 
 // don't touch below this line
 
-func sendIsOld(isOldChan chan<- bool, emails [3]email) {
-	for _, e := range emails {
-		if e.date.Before(time.Date(2020, 0, 0, 0, 0, 0, 0, time.UTC)) {
-			isOldChan <- true
-			continue
+func getDBsChannel(numDBs int) (chan struct{}, *int) {
+	count := 0
+	ch := make(chan struct{})
+
+	go func() {
+		for i := range numDBs {
+			ch <- struct{}{}
+			fmt.Printf("Database %v is online\n", i+1)
+			count++
 		}
-		isOldChan <- false
-	}
+	}()
+
+	return ch, &count
 }
 
 func main() {
 	fmt.Println("app started")
+	
 }
