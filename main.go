9254d4bc33e248253ee1f4ff3775554ffd2abb12
Ej <elnesjani@gmail.com>
2025-10-19 23:02:39 +0800

    finish new lesson

--- main.go
+++ main.go
@@ -51,30 +51,96 @@ import (
 // 	}
 // }
 
-func waitForDBs(numDBs int, dbChan chan struct{}) {
-	for range numDBs{
-		<-dbChan
+// ------------------------------------------------------ lesson 3
+
+// func waitForDBs(numDBs int, dbChan chan struct{}) {
+// 	for range numDBs{
+// 		<-dbChan
+// 	}
+// }
+
+// // don't touch below this line
+
+// func getDBsChannel(numDBs int) (chan struct{}, *int) {
+// 	count := 0
+// 	ch := make(chan struct{})
+
+// 	go func() {
+// 		for i := range numDBs {
+// 			ch <- struct{}{}
+// 			fmt.Printf("Database %v is online\n", i+1)
+// 			count++
+// 		}
+// 	}()
+
+// 	return ch, &count
+// }
+
+// ------------------------------------------------- lesson 4
+
+// func addEmailsToQueue(emails []string) chan string {
+// 	emailMsg := make(chan string, len(emails))
+
+// 	for _, em := range emails{
+// 		emailMsg <- em
+// 	}
+
+// 	close(emailMsg)
+
+// 	return emailMsg
+// }
+
+// ------------------------------------------------- lesson 5
+
+// func countReports(numSentCh chan int) int {
+// 	count := 0;
+
+// 	for {
+// 		ch, ok := <- numSentCh;
+// 		if  !ok {
+// 			break
+// 		}
+// 		count += ch
+// 	}
+
+// 	return count
+// }
+
+// // don't touch below this line
+
+// func sendReports(numBatches int, ch chan int) {
+// 	for i := range numBatches {
+// 		numReports := i*23 + 32%17
+// 		ch <- numReports
+// 	}
+// 	close(ch)
+// }
+
+// ------------------------------------------------- lesson 6
+
+func concurrentFib(n int) []int {
+	s := make(chan int)
+	intSlice := make([]int,0)
+	go fibonacci(n,s)
+
+	for val := range s{
+		intSlice = append(intSlice, val)
 	}
+	
+	return intSlice
 }
 
 // don't touch below this line
 
-func getDBsChannel(numDBs int) (chan struct{}, *int) {
-	count := 0
-	ch := make(chan struct{})
-
-	go func() {
-		for i := range numDBs {
-			ch <- struct{}{}
-			fmt.Printf("Database %v is online\n", i+1)
-			count++
-		}
-	}()
-
-	return ch, &count
+func fibonacci(n int, ch chan int) {
+	x, y := 0, 1
+	for i := 0; i < n; i++ {
+		ch <- x
+		x, y = y, x+y
+	}
+	close(ch)
 }
 
 func main() {
 	fmt.Println("app started")
-	
 }
